68654a01ecc5ea5082857b1a3d14e7fc69dd3b65
t-anikaa <161161157+t-anikaa@users.noreply.github.com>
2024-02-26 01:34:28 +0600

    Uploaded Class 1 file.

---
+++ Class 1.js
@@ -0,0 +1,47 @@
+1. Write code in JS: 
+Declare two variables, then assign your first name and last name. Finally, print your full name including one space between both names.
+
+var firstName = "Anika";
+var lastName = "Tahsin";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+The output will be Anika Tahsin.
+
+2. Write code in JS:    Suppose a = 5, b = "60". Use type conversion and print the sum of the two variables.
+
+var a = 5;
+var b = "60";
+var bNumber = parseInt(b);
+console.log(bNumber);
+//the output will be 60
+
+Again,
+var sum = a + bNumber;
+console.log(sum);
+the output will be 65.
+
+3. Write code in JS: Num1 = 59, txt1 = "hello JS". Compare both variables and print the result.
+
+var Num1 = 59;
+var txt1 = "hello JS";
+console.log(Num1 == txt1);
+
+the output will be false.
+
+4. Write code in JS: Num2 = 344, txtNum = "344". Compare the value of both variables along with the data type and print the result.
+
+var Num2 = 344;
+var txtNum = "344";
+console.log(Num2 ===txtNum);
+
+the output will be false as Num2 is a number type data and txtNum is string type data
+
+5. Run the below code in JS, write your output:
+
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
+
+The output will be true.
